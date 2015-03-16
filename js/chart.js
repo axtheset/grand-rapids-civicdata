cbfa9be5608f2275a0ab63b70711c42e5574e3fb
axtheset <sethaxthelm@gmail.com>
2015-03-16 14:15:33 -0400

    Adding word cloud to the dashboard

--- js/chart.js
+++ js/chart.js
@@ -25,8 +25,9 @@
         $('.canvas').hide();
 
         var resourceId = "e741edf8-04ad-450d-bc62-6684a7a427dd";
-        var baseURI = "http://www.civicdata.com/api/action/datastore_search_sql?sql=";
-         
+        //var baseURI = "http://www.civicdata.com/api/action/datastore_search_sql?sql=";
+        var baseURI = "http://civicdataprod1.cloudapp.net/api/action/datastore_search_sql?sql=";
+        
         // Helper function to make request for JSONP.
         function requestJSON(url, callback) {
           $.ajax({
@@ -60,7 +61,6 @@
               neighborhoods.push(records[i].neighborhood);
           }
 
-          console.log(neighborhoods);
 
           var months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
           var dataColumns = new Array();
@@ -92,8 +92,6 @@
             }
             chartData.push(tempArray);
           }
-
-          console.log(chartData);
 
             var chart = c3.generate({
               bindto: '#chart',
@@ -320,4 +318,83 @@
               
           });
 
+        var permitDescQuery = "SELECT \"Description\" from \"resource_id\" WHERE \"Applied Online\" != \'\' and \"Issued\" >= \'2014-01-01\'";
+
+        var permitDesc = baseURI + encodeURIComponent(permitDescQuery.replace("resource_id",resourceId));
+
+
+        requestJSON(permitDesc, function(json) {
+            var descriptions = json.result.records;
+
+            var descString = "";
+
+            descriptions.forEach(function(d) {
+              descString += d.Description + " ";
+            });
+
+            var descArray = descString.split(" ");
+
+            var descObjects = [];
+            descArray.forEach(function(d) {
+              if (!isNumeric(d) && !matches(d,"AND","OF","TO","","&","ON","-","THE","IN","BE","FOR","A")) {
+                var descObject = {}
+                descObject.description = d;
+                descObjects.push(descObject);
+              }
+            });
+                      
+            var wordCount = d3.nest()
+              .key(function(d) { return d.description; })
+              .rollup(function(v) { return v.length; })
+              .entries(descObjects);
+
+            wordCount.sort(function(a,b) {
+              return b.values - a.values;
+            });    
+
+            var tags = [];
+
+            wordCount.forEach(function(d) {
+              tags.push([d.key,parseInt(d.values)]);
+            });
+
+
+
+            tags = tags.slice(0,250);
+
+            
+            WordCloud(document.getElementById('cloud'), {
+              list : tags.map(function(word) { return [word[0], Math.round(word[1]/5)]; })
+            });
+
+            //console.log(tagMap);
+            
+            var clicked = function(ev) {
+              if (ev.target.nodeName === "SPAN") {
+                var tag = ev.target.textContent;
+                var tagElem;
+                if (tags.some(function(el) { if (el[0] === tag) {tagElem = el; return true;} return false; })) {
+                document.getElementById("details").innerText = "There were " + tagElem[1] + 
+                    " mentions in permit descriptions of “" + tag + "” in the last year";
+                }
+              } else {
+                document.getElementById("details").innerText = "";
+              }
+            }
+            document.getElementById("cloud").addEventListener("click", clicked)
+              
+          });
+
+
+
       });
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function matches(eVal,argList) {
+   for (var i=1; i<arguments.length;i++)
+    if (arguments[i] == eVal)
+      return true;
+}
